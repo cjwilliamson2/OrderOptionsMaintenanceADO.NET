87a41fd43689796dbfb20c82a2349840bd1eea51
cjwilliamson2 <cjwilliamson2@pstcc.edu>
2024-04-25 11:15:46 -0400

    Implemented Get and Save functions from database

--- OrderOptionsMaintenance/Models/MMABooksDataAccess.cs
+++ OrderOptionsMaintenance/Models/MMABooksDataAccess.cs
@@ -1,15 +1,66 @@
-﻿namespace OrderOptionsMaintenance.Models
+﻿using Microsoft.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace OrderOptionsMaintenance.Models
 {
     public class MMABooksDataAccess
     {
+
+        private string ConnectionString => ConfigurationManager.ConnectionStrings["MMABooks"].ConnectionString;
+
         public OrderOption GetOrderOptions()
         {
-            throw new NotImplementedException();
+            using SqlConnection connection = new(ConnectionString);
+
+            string select = "SELECT OptionID, SalesTaxRate, FirstBookShipCharge, AdditionalBookShipCharge " +
+                            "FROM OrderOptions";
+
+            using SqlCommand command = new SqlCommand(select, connection);
+            connection.Open();
+
+            OrderOption options = null!;
+
+            using SqlDataReader reader = command.ExecuteReader(CommandBehavior.SingleRow & CommandBehavior.CloseConnection);
+
+            if (reader.Read())
+            {
+                options = new OrderOption
+                {
+                    OptionId = (int)reader["OptionID"],
+                    SalesTaxRate = (decimal)reader["SalesTaxRate"],
+                    FirstBookShipCharge = (decimal)reader["FirstBookShipCharge"],
+                    AdditionalBookShipCharge = (decimal)reader["AdditionalBookShipCharge"]
+                };
+            }
+
+            return options;
         }
 
         public void SaveOrderOptions(OrderOption options)
         {
+            try
+            {
+                using SqlConnection connection = new(ConnectionString);
 
+                string update = "UPDATE OrderOptions SET " +
+                                "SalesTaxRate = @SalesTaxRate, " +
+                                "FirstBookShipCharge = @FirstBookShipCharge, " +
+                                "AdditionalBookShipCharge = @AdditionalBookShipCharge";
+
+                using SqlCommand command = new(update, connection);
+
+                command.Parameters.AddWithValue("@SalesTaxRate", options.SalesTaxRate);
+                command.Parameters.AddWithValue("@FirstBookShipCharge", options.FirstBookShipCharge);
+                command.Parameters.AddWithValue("@AdditionalBookShipCharge", options.AdditionalBookShipCharge);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                throw new DataAccessException(ex.Message, "Update Error");
+            }
         }
     }
 
